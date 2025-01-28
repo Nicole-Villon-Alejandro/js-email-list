614ed90dc788e53bfff972ec257fccfc9f434689
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-01-28 17:13:32 +0100

    add:changes

--- main.js
+++ main.js
@@ -9,11 +9,12 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 fetch("https://flynn.boolean.careers/exercises/api/random/mail")
 .then(response => response.json())
 .then(data => {
-    // codice per far qualcosa con la risposta
+
     console.log(data.response);
     risultato.innerHTML = data.response
 
 })
+
 .catch(error => {
     // codice da eseguire in caso di errore
     console.error(error);
