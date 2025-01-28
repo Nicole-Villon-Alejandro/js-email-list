22082cb146569f8fac5972727bee0b5d971151f1
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-01-28 17:11:21 +0100

    add:ex

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api
 Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)*/
+
+//Endpoint: https://flynn.boolean.careers/exercises/api/random/mail
+
+fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+.then(response => response.json())
+.then(data => {
+    // codice per far qualcosa con la risposta
+    console.log(data.response);
+    risultato.innerHTML = data.response
+
+})
+.catch(error => {
+    // codice da eseguire in caso di errore
+    console.error(error);
+});
+
+
+
